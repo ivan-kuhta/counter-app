6d58e8b15f062ac9f9c28d85bb3cd7e6a7f8687b
Ivan <Ivan1998pi22342@ukr.net>
2020-07-05 16:19:59 +0300

    Start Redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import * as actions from './actions';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(reducer);
+const {dispatch} = store;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+// const incDispatch = () => dispatch(inc());
+// const decDispatch = () => dispatch(dec());
+// const resDispatch = () => dispatch(res());
+
+const {inc, dec, res} = bindActionCreators(actions, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const resDispatch = bindActionCreators(res, dispatch);
+
+
+document.getElementById('inc').addEventListener('click', () => {
+  inc();
+})
+document.getElementById('dec').addEventListener('click', () => {
+  dec();
+})
+document.getElementById('reset').addEventListener('click', () => {
+  res();
+})
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
